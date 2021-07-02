a73be91f1c361134d18eadaf220c4c23d4b236ff
Amanda Pipich <79532564+apipich@users.noreply.github.com>
2021-07-02 16:46:19 -0400

    created functions to pull value of search

--- src/Search.js
+++ src/Search.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Search.css";
 
 export default function Search() {
+let [word, setWord] = useState("");
+
+function showWord(event) {
+setWord(event.target.value);
+}
+
+  function locate(event) {
+    event.preventDefault();
+alert(`Searching for ${word}`);
+  }
+  
 return (
 <div className="dictionary">
-<form>
-  <input type="search" />
+<form onSubmit={locate}>
+  <input type="search" autoFocus={true} onChange={showWord}/>
   <input type="submit" value="search"/>
 </form>
 </div>
