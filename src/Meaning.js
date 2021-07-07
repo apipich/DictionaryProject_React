dd03b71faea6a79cb0dc4c0a4441f3533dff65c3
Amanda Pipich <79532564+apipich@users.noreply.github.com>
2021-07-07 14:29:22 -0400

    pulled word definition/example from api

--- src/Meaning.js
+++ src/Meaning.js
@@ -2,8 +2,20 @@ import React from "react";
 
 export default function Meaning(props) {
 return (
-  <div>
-    hello
+  <div className="details">
+    <h3>{props.meaning.partOfSpeech}</h3>
+{props.meaning.definitions.map(function(definition, index) {
+return (
+  <div key={index}>
+    <p>
+{definition.definition}
+{definition.example}
+</p>
+</div>
+)
+
+})}
+
     </div>
 )
 }
