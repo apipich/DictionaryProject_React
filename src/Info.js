d94859d4252f126765e215746810a5d31916c7f5
Amanda Pipich <79532564+apipich@users.noreply.github.com>
2021-07-10 14:52:40 -0400

    created new component Phonetic

--- src/Info.js
+++ src/Info.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Meaning from "./Meaning.js";
+import Phonetic from "./Phonetic.js";
 
 export default function Info(props) {
   console.log(props.info);
@@ -8,6 +9,14 @@ return (
   <div className="information">
     <h2>{props.info.word}
     </h2>
+{props.info.phonetics.map(function(phonetic, index) {
+return (
+<div key={index}>
+  <Phonetic phonetic={phonetic} />
+  </div>
+);
+})}
+
 {props.info.meanings.map(function(meaning, index) {
 return (
 <div key={index}>
