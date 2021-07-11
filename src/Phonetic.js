ac2f584ada57f7063ff0b1400c71344aedbba472
Amanda Pipich <79532564+apipich@users.noreply.github.com>
2021-07-11 13:52:45 -0400

    added extra space between audio and text phonetics

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -5,7 +5,7 @@ export default function Phonetic(props) {
   return (
     <div className="phonetic">
     <a href={props.phonetic.audio} target="_blank" rel="noopener noreferrer"><i className="fas fa-headphones"></i></a>
-<span className="text">{props.phonetic.text}</span>
+    &nbsp; &nbsp;<span className="text">{props.phonetic.text}</span>
     </div>
   )
 }
