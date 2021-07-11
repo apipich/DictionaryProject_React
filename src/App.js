a208fbc26acb76045fdae2a043b2f47ee4321d43
Amanda Pipich <79532564+apipich@users.noreply.github.com>
2021-07-11 10:41:39 -0400

    created function/new state to show default keyword

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
     <div className="App">
       <div className="container">
       <h1>Dictionary Project</h1>
-<Search/>
+<Search defaultKeyword="forest"/>
     </div>
     <footer className="myCode"><small><a href="https://github.com/apipich/DictionaryProject_React" target="_blank" rel="noopener noreferrer">Open-source code</a> by Amanda Pipich</small></footer>
     </div>
--- src/Search.js
+++ src/Search.js
@@ -3,32 +3,48 @@ import axios from "axios";
 import Info from "./Info.js";
 import "./Search.css";
 
-export default function Search() {
-let [keyword, setKeyword] = useState("");
+export default function Search(props) {
+let [keyword, setKeyword] = useState(props.defaultKeyword);
 let [info, setInfo] = useState(null);
+let [loaded, setLoad] = useState(false);
 
 function showResponse(response) {
 setInfo(response.data[0]);
 }
 
-function locate(event) {
-event.preventDefault();
-
+function search() {
 let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
 axios.get(apiUrl).then(showResponse);
 }
 
 function handleSubmit(event) {
+event.preventDefault();
+search();
+}
+
+function handleWord(event) {
 setKeyword(event.target.value);
 }
 
+function load() {
+  setLoad(true);
+search();
+}
+
+if (loaded) {
 return (
 <div className="dictionary">
-<form onSubmit={locate}>
-  <input type="search" autoFocus={true} onChange={handleSubmit}/>
+<form onSubmit={handleSubmit}>
+  <input type="search" autoFocus={true} onChange={handleWord}/>
   <input type="submit" value="search"/>
 </form>
 <Info info={info}/>
 </div>
 ) 
+} else {
+load();
+return "Loading...";
 }
+}
+
+
