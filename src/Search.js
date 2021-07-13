74fccd0f374184a892b451e0c5212b4f5f58bee6
Amanda Pipich <79532564+apipich@users.noreply.github.com>
2021-07-13 13:14:30 -0400

    added Pexel api to render photos - created new component Photos to render photos at bottom of app - styled photos

--- src/Search.js
+++ src/Search.js
@@ -1,20 +1,31 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Info from "./Info.js";
+import Photos from "./Photos.js";
 import "./Search.css";
 
 export default function Search(props) {
 let [keyword, setKeyword] = useState(props.defaultKeyword);
 let [info, setInfo] = useState(null);
 let [loaded, setLoad] = useState(false);
+let [photos, setPhotos] = useState(null);
 
 function showResponse(response) {
 setInfo(response.data[0]);
 }
 
+function showPexelResponse(response) {
+setPhotos(response.data.photos);
+}
+
 function search() {
 let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
 axios.get(apiUrl).then(showResponse);
+
+let pexelApiKey = "563492ad6f917000010000012b167b8f729e4031a7395066e2ef4ac4";
+let pexelApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`
+let headers = { Authorization: `Bearer ${pexelApiKey}` };
+axios.get(pexelApiUrl, { headers: headers }).then(showPexelResponse);
 }
 
 function handleSubmit(event) {
@@ -43,6 +54,7 @@ return (
 </section>
 <br />
 <Info info={info}/>
+<Photos photos={photos}/>
 </div>
 ) 
 } else {
