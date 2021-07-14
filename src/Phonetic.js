4707f4552d591c12ab33d9ec49db53b6b123b3ed
Amanda Pipich <79532564+apipich@users.noreply.github.com>
2021-07-14 14:04:54 -0400

    added styling to phonetics button

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -4,8 +4,11 @@ import "./Phonetic.css";
 export default function Phonetic(props) {
   return (
     <div className="phonetic">
-    <a href={props.phonetic.audio} target="_blank" rel="noopener noreferrer"><i className="fas fa-headphones"></i></a>
+<a className="headphones" href={props.phonetic.audio} target="_blank" rel="noopener noreferrer">
+  <i className="fas fa-headphones"></i></a>
     &nbsp; &nbsp;<span className="text">{props.phonetic.text}</span>
     </div>
   )
 }
+
+
